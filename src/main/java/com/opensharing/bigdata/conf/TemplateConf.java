966572aaa9e9d389688f4a4b9c21051495ac040b
ludengke95 <ludengke95@gmail.com>
2019-12-18 19:38:54 +0800

    增加配置配的注释

--- src/main/java/com/opensharing/bigdata/conf/TemplateConf.java
+++ src/main/java/com/opensharing/bigdata/conf/TemplateConf.java
@@ -4,7 +4,25 @@ package com.opensharing.bigdata.conf;
  * 模板配置枚举类
  */
 public enum TemplateConf {
-    APP_NAME("app_name"), DURATION("duration"), MASTER("master"),KRYO_CLASSES("kryo_classes");
+    /**
+     * 设置spark 应用的名称
+     */
+    APP_NAME("app_name"),
+
+    /**
+     * 本地执行的时候需要指定setMaster
+     */
+    MASTER("master"),
+
+    /**
+     * 设置Streaming的启动时间间隔，value是Duration对象或者整数
+     */
+    DURATION("duration"),
+
+    /**
+     * 用于设置启用kryo序列化的类
+     */
+    KRYO_CLASSES("kryo_classes");
 
     TemplateConf(String value) {
         this.value = value;
